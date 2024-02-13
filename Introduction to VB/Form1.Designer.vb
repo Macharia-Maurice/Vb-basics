76975b821f8081005491ce08e8b9cd5344f98502
Macharia-Maurice <maurice.maina@students.jkuat.ac.ke>
2024-02-13 07:22:33 +0300

    welcome form

--- Introduction to VB/Form1.Designer.vb
+++ Introduction to VB/Form1.Designer.vb
@@ -22,10 +22,57 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        components = New ComponentModel.Container()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        ContextMenuStrip2 = New ContextMenuStrip(components)
+        RichTextBox1 = New RichTextBox()
+        RichTextBox2 = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' ContextMenuStrip2
+        ' 
+        ContextMenuStrip2.Name = "ContextMenuStrip2"
+        ContextMenuStrip2.Size = New Size(61, 4)
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        RichTextBox1.Location = New Point(128, 191)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(171, 47)
+        RichTextBox1.TabIndex = 3
+        RichTextBox1.Text = "Load Object Demo"
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.Font = New Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        RichTextBox2.ForeColor = SystemColors.InfoText
+        RichTextBox2.Location = New Point(377, 191)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.Size = New Size(171, 47)
+        RichTextBox2.TabIndex = 4
+        RichTextBox2.Text = "Exit"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 527)
+        Controls.Add(RichTextBox2)
+        Controls.Add(RichTextBox1)
+        Name = "Form1"
+        Text = "Welcome Form"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ContextMenuStrip2 As ContextMenuStrip
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
 
 End Class
--- Introduction to VB/Form1.vb
+++ Introduction to VB/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
